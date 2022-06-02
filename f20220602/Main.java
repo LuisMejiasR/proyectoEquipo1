54ff60f5c938c24163c6fd7ee5880815793249ed
LuisMejiasR <luisme.1993@gmail.com>
2022-06-02 11:29:12 -0400

    ejercicio herencia Equipo1

---
+++ f20220602/Main.java
@@ -0,0 +1,14 @@
+package com.generation.f20220602;
+
+public class Main {
+	public static void main(String[] args) {
+		//
+		VehiculoTerrestre Vehiculo = new VehiculoTerrestre(4,"Rojo","Con Motor",5,true,"01");
+		Camioneta Tundra = new Camioneta();
+		Moto Susuki = new Moto();
+		
+		
+		
+		
+	}
+}
